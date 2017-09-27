2b4b6478bf19756f6a66f9c677ff70d9f4442801
Puneet <puneetgupta18190@gmail.com>
2017-09-27 11:54:02 +0000

    fc

--- config/application.rb
+++ config/application.rb
@@ -4,17 +4,17 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
+Bundler.require(platform)
 
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Europe/Warsaw'
-	# optional - note it can be only :utc or :local (default is :utc)
-	config.active_record.default_timezone = :local
-	platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
-	Bundler.require(platform)
+    # optional - note it can be only :utc or :local (default is :utc)
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
